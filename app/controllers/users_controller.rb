2affeb742ebc34416dde55d229c80777f321929f
afrin <titlee42@gmail.com>
2018-07-29 15:57:10 +0600

    fifth update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,11 @@
 class UsersController < ApplicationController
   
-  before_action :logged_in_user, only: [:edit, :update]
-  before_action :correct_user,   only: [:edit, :update]
+  # before_action :logged_in_user, only: [:edit, :update]
+  # before_action :correct_user,   only: [:edit, :update]
 
   def index
     @users = User.all
+    
   end
 
   def show
@@ -32,18 +33,18 @@ class UsersController < ApplicationController
   	end
   end
 
-def logged_in_user
-  unless logged_in?
-    flash[:danger]= "Please Log in"
-    redirect_to login_url
-  end
-end
+# def logged_in_user
+#   unless logged_in?
+#     flash[:danger]= "Please Log in"
+#     redirect_to login_url
+#   end
+# end
 
-def correct_user
-       @user = User.find(params[:id])
-      redirect_to(root_url) unless current_user?(@user)
+# def correct_user
+#        @user = User.find(params[:id])
+#       redirect_to(root_url) unless current_user?(@user)
       
-    end
+#     end
   
 
   private
